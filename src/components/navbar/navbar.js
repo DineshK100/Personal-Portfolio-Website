be8614e87f8854b75486fe7bc7910cb22cb2fc21
drkarnat <drkarnat@ncsu.edu>
2024-07-26 16:50:12 -0400

    Designed the landing page

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,39 +1,72 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-scroll";
 import "./navbar.css";
 
 const Navbar = () => {
-    useEffect(() => {
-        const handleScroll = () => {
-          const navEl = document.querySelector(".navbar");
-          if (window.scrollY >= 56) {
-            navEl.classList.add("navbar-scrolled");
-          } else {
-            navEl.classList.remove("navbar-scrolled");
-          }
-        };
-    
-        window.addEventListener("scroll", handleScroll);
-        return () => {
-          window.removeEventListener("scroll", handleScroll);
-        };
-      }, []);
+  useEffect(() => {
+    const handleScroll = () => {
+      const navEl = document.querySelector(".navbar");
+      if (window.scrollY >= 56) {
+        navEl.classList.add("navbar-scrolled");
+      } else {
+        navEl.classList.remove("navbar-scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <div>
-      <nav className="navbar">
-        <div className="navbar-content">
-        <div className="name">
-          <p>Dinesh Karnati</p>
+      <nav class="navbar">
+        <div class="navbar-content">
+          <div class="name">Dinesh Karnati</div>
+          <div class="desktopmenu">
+            <a class="desktopMenuListItem" href="#home">
+              Home
+            </a>
+            <a class="desktopMenuListItem" href="#about">
+              About
+            </a>
+            <a class="desktopMenuListItem" href="#projects">
+              Projects
+            </a>
+            <a class="desktopMenuListItem" href="#skills">
+              Skills
+            </a>
+            <a class="desktopMenuListItem" href="#experience">
+              Experience
+            </a>
+            <a class="desktopMenuListItem" href="#social-media">
+              Social Media
+            </a>
+          </div>
+          <button class="dropdown-toggle" aria-label="Toggle menu">
+            &#9776;
+          </button>
         </div>
-        <div className="desktopmenu">
-          <a href = "#intro-wrapper" className="desktopMenuListItem" >Home</a>
-          <a className="desktopMenuListItem" >About</a>
-          <a className="desktopMenuListItem">Projects</a>
-          <a className="desktopMenuListItem">Skills</a>
-          <a className="desktopMenuListItem">Experience</a>
-          <a className="desktopMenuListItem">Social Media</a>
-        </div>
+        <div class="dropdown-menu">
+          <a class="dropdownMenuItem" href="#home">
+            Home
+          </a>
+          <a class="dropdownMenuItem" href="#about">
+            About
+          </a>
+          <a class="dropdownMenuItem" href="#projects">
+            Projects
+          </a>
+          <a class="dropdownMenuItem" href="#skills">
+            Skills
+          </a>
+          <a class="dropdownMenuItem" href="#experience">
+            Experience
+          </a>
+          <a class="dropdownMenuItem" href="#social-media">
+            Social Media
+          </a>
         </div>
       </nav>
     </div>
--- src/intro.js
+++ src/intro.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./intro.css";
 
+import About from "./about";
+
 const Intro = () => {
   return (
     <div className="page-background">
@@ -13,27 +15,34 @@ const Intro = () => {
           </div>
         </section>
 
-          <div class="image-1">
-            <img alt="headshot" src="./headshot.jpeg" />
-          </div>
-          <div class="image-2">
-            <img alt="another pic" src="./headshot.jpeg" />
-          </div>
-          <div class="image-3">
-            <img alt="other pic pic" src="./headshot.jpeg" />
-          </div>
-          <div class="image-4">
-            <img alt="other pic pic" src="./headshot.jpeg" />
-          </div>
-
-          <div>
-            
-          </div>
+        <div class="image-1"> x
+          <img alt="headshot" src="./headshot.jpeg" draggable="false"/>
+        </div>
+        <div class="image-2">
+          <img alt="another pic" src="./headshot.jpeg" draggable="false"/>
+        </div>
+        <div class="image-3">
+          <img alt="other pic pic" src="./headshot.jpeg" draggable="false"/>
+        </div>
+        <div class="image-4">
+          <img alt="other pic pic" src="./headshot.jpeg" draggable="false"/>
+        </div>
+      </div>
+      <div className="motivational-quote">
+        <h1>
+          "There will be obstacles. There will be doubters. There will be
+          mistakes. But with hard work, there are no limits."
+        </h1>
+      </div>
+      <div className="buttons">
+        <button className = "buttons-display" type="button">Portfolio</button>
+        <button className = "buttons-display" type="button">Resume</button>
       </div>
 
-    
+      <About/>
     </div>
   );
 };
 
 export default Intro;
+//1065
